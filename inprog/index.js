18b1e4103e285ea7195efab94c99f70a5fc69f82
Raechel Portelli <88565111+raechelportelli@users.noreply.github.com>
2021-10-20 17:28:22 -0400

    Update index.js

--- inprog/index.js
+++ inprog/index.js
@@ -13,7 +13,7 @@ const config = {
   /**
    * The layer within the vector tileset to use for querying
    */
-  sourceLayer: 'kabUpd-da0558',
+  sourceLayer: 'kabupaten-9wp6sx',
   /**
    * This sets the title in the sidebar and the <title> tag of the app
    */

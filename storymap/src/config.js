60067493d439785b95b6773a0c94292915b3b0ce
Raechel Portelli <88565111+raechelportelli@users.noreply.github.com>
2021-11-27 18:44:28 -0500

    Update config.js

--- storymap/src/config.js
+++ storymap/src/config.js
@@ -49,7 +49,7 @@ var config = {
       description:
         "The Salem Witch Trials began in the spring of 1692. The first victim, Bridget Bishop, was hung on June 10th. By the fall, over 150 men, women, and children were accused of witchcraft. By spring of 1693, those imprisonated for witchcraft were exonerated. However, the final death toll included 19 were hanged on Gallows Hill, an elderly man was pressed to death, and several others died in jail.",
       location: {
-        center: [42.53, -70.7866],
+        center: [-70.7866, 42.53],
         zoom: 4.75,
          pitch: 60,
          bearing: 0

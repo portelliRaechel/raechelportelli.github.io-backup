265d13c4454bd2170830e7a5ac2850a7f491d5c7
Curmudgeon, PhD <raechel.portelli@gmail.com>
2021-12-03 10:22:34 -0500

    maps

--- javaAtlas/trucks.js
+++ javaAtlas/trucks.js
@@ -1,15 +1,8 @@
-var marker = new mapboxgl.Marker()
-  .setLngLat([-21.9270884, 64.1436456])
-  .addTo(map);
-
-var popup = new mapboxgl.Popup().setHTML(
-  "<h3>Reykjavik Roasters</h3><p>A good coffee shop</p>"
-);
-
-map.on("load", function () {
-  map.addSource("mapbox-dem", {
-    type: "raster-dem",
-    url: "mapbox://mapbox.mapbox-terrain-dem-v1",
-  });
-  map.setTerrain({ source: "mapbox-dem" });
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiY3VybXVkZ2VvbnBoZCIsImEiOiJja3VxNjkxN2k0c2w0MnptbjU0N242cHFzIn0.DI3yxzJz-CO25QvNLXESOQ";
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v11", // style URL
+  center: [-74.5, 40], // starting position [lng, lat]
+  zoom: 9, // starting zoom
 });

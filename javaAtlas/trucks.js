da447ff20205dddad64dbcc49e7294d854140867
Curmudgeon, PhD <raechel.portelli@gmail.com>
2021-12-03 10:16:28 -0500

    map trucks

--- javaAtlas/trucks.js
+++ javaAtlas/trucks.js
@@ -1,10 +1,3 @@
-var map = new mapboxgl.Map({
-  container: "map", // HTML container id
-  style: "mapbox://styles/mapbox/streets-v9", // style URL
-  center: [-21.9270884, 64.1436456], // starting position as [lng, lat]
-  zoom: 13,
-});
-
 var marker = new mapboxgl.Marker()
   .setLngLat([-21.9270884, 64.1436456])
   .addTo(map);
